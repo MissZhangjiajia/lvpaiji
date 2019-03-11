8628964bc689afbbd5f9bfce1ce8881676de8c24
zjj <zjj@163.com>
2019-03-11 16:12:28 +0800

    firstdev

--- src/components/Remai.js
+++ src/components/Remai.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+const Search = Input.Search;
 class Remai extends React.Component{
     render(){
         return(
             <div className="container">
+                <Search
+                placeholder="input search text"
+                onSearch={value => console.log(value)}
+                style={{ width: 200 }}
+              />
             </div>
         )
     }
